b5ba4ea431bbd521aa435baf9347ee1318e93422
EChaban <4chaban@gmail.com>
2022-08-20 17:39:15 +0300

    Create Task 1.cs

---
+++ Task 1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+class Form {
+    static void Main()
+    {
+        string name = Interaction.InputBox("What's your name?", "Name");
+        string surname = Interaction.InputBox("What's your last name?", "Last name");
+        int years = Int32.Parse(Interaction.InputBox("What is your year of birth?", "Year of birth"));
+        int years2 = 2022 - years;
+        MessageBox.Show("Is it right? "+name+" "+surname+" "+years2+ " years old.", "Your form", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+    }
+}
