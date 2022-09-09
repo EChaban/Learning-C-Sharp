2aa3c2b67303d69e93c7ec62a916f9d442af22f2
EChaban <4chaban@gmail.com>
2022-09-09 12:22:21 +0300

    Lesson 2-3

---
+++ Lesson 2-3/CodeFile1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Security.Policy;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+using static System.Net.Mime.MediaTypeNames;
+
+class Name
+{
+    static void Main()
+    {
+        try
+        {
+            int n = Int32.Parse(Interaction.InputBox("Enter the number", "Programm"));
+            int num1 =1;
+            int k = 0;
+            int sum = 0;
+            string text = ""; 
+            while (k < n)
+            {
+                if (num1%5==2 || num1%3 == 1)
+                {
+                    k++;
+                    text += num1+" ";
+                    sum += num1;
+                    num1++;
+                }
+                else
+                {
+                    num1++;
+                }
+            }
+            MessageBox.Show( text+"="+sum, "Programm", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+        catch
+        {
+            MessageBox.Show("An error has occurred", "Programm", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+    }
+}
