e35a492e8e4f29e635c41f8bd156db1a87b08feb
Eugene Chaban <4chaban@gmail.com>
2024-07-23 13:12:53 +0300

    23.07.24

---
+++ C# Book №1 - (Васильев А.Н.)/Глава 7. Работа с текстом/Homework 1/Homework 1.cs
@@ -0,0 +1,26 @@
+﻿/**/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "text";
+            string b = "Text";
+            
+            Console.WriteLine(a.Equals(b, StringComparison.OrdinalIgnoreCase));
+            Console.WriteLine(String.Equals(a, b, StringComparison.OrdinalIgnoreCase));
+            Console.WriteLine(a.ToLower() == b.ToLower());
+
+
+        }
+    }
+}
