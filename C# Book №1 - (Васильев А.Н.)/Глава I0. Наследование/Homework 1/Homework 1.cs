8e6648a12c93e571d2c5872df3130ce14bfdf22c
Eugene Chaban <4chaban@gmail.com>
2024-09-12 18:53:13 +0300

    Глава I0. Наследование

---
+++ C# Book №1 - (Васильев А.Н.)/Глава I0. Наследование/Homework 1/Homework 1.cs
@@ -0,0 +1,62 @@
+﻿/*Напишите программу, в которой есть базовый класс с защищенным
+текстовым полем, конструктором с текстовым аргументом и где переопределен
+метод ToString(). На основе базового класса путем наследования
+создается производный класс. У него появляется еще одно защищенное
+текстовое поле. Также производный класс должен иметь версии
+конструктора с одним и двумя текстовыми аргументами, а еще в нем
+должен быть переопределен метод ToString(). В обоих классах метод
+ToString() переопределяется так, что он возвращает строку с названием
+класса и значение текстового поля или текстовых полей.*/
+
+using System;
+
+namespace Homework_1
+{
+    class MyBaseClass
+    {
+        protected string text;
+
+        public MyBaseClass(string text)
+        {
+            this.text = text;
+        }
+
+        public override string ToString(){
+            Console.Write("MyBaseClass ");
+            return text;
+        }
+    }
+
+    class InheritClass : MyBaseClass
+    {
+        protected string secondText;
+
+        public InheritClass(string text) : base (text)
+        {
+            this.secondText = text;
+        }
+
+        public InheritClass(string text, string secondText) : base(text)
+        {
+            this.secondText = secondText;
+        }
+
+        public override string ToString()
+        {
+            Console.Write($"InheritClass {text} ");
+            return secondText;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyBaseClass a = new MyBaseClass("text 1");
+            Console.WriteLine(a);
+            InheritClass b = new InheritClass("text 2");
+            Console.WriteLine(b);
+            InheritClass c = new InheritClass("text 3", "text 4");
+            Console.WriteLine(c);
+        }
+    }
+}
---
+++ C# Book №1 - (Васильев А.Н.)/Глава I0. Наследование/Homework 2/Homework 2.cs
@@ -0,0 +1,125 @@
+﻿/*Напишите программу, в которой есть базовый класс с защищенным
+текстовым полем. В базовом классе должен быть метод для присваивания
+значения полю: без аргументов и с одним текстовым аргументом.
+Объект базового класса создается передачей одного текстового аргумента
+конструктору. Доступное только для чтения свойство результатом
+возвращает длину текстовой строки. Доступный только для чтения индексатор
+возвращает значением символ из текстовой строки. На основе
+базового класса создается производный класс. В производном классе появляется
+дополнительное открытое целочисленное поле. В классе должны
+быть такие версии метода для присваивания значений полям (используется
+переопределение и перегрузка метода из базового класса) :
+без аргументов, с текстовым аргументом, с целочисленным аргументом,
+с текстовым и целочисленным аргументом. У конструктора производного
+класса два аргумента (целочисленный и текстовый).*/
+
+using System;
+
+namespace Homework_2
+{
+    class BaseClass
+    {
+        protected string text;
+
+        public BaseClass(string text)
+        {
+            this.text = text;
+        }
+
+        public virtual void Setter()
+        {
+            text = "text";
+        }
+        public virtual void Setter(string text)
+        {
+            this.text = text;
+        }
+
+        public int GetLength
+        {
+            get
+            {
+                return text.Length;
+            }
+        }
+
+        public char this[int i]
+        {
+            get
+            {
+                return text[i];
+            }
+        }
+
+        public string GetText()
+        {
+            string text = "";
+
+            for (int i = 0; i < this.GetLength; i++)
+            {
+                text += this[i];
+            }
+            return text;
+        }
+    }
+
+    class InheritClass : BaseClass
+    {
+        public int num;
+
+        public InheritClass(int num, string text) : base(text)
+        {
+            this.num = num;
+            this.text = text;
+        }
+        public override void Setter()
+        {
+            text = "text";
+            num = 0;
+        }
+
+        public void Setter(int num)
+        {
+            text = "text";
+            this.num = num;
+        }
+
+        public override void Setter(string text)
+        {
+            this.text = text;
+            num = 0;
+        }
+
+        public void Setter(string text, int num)
+        {
+            this.text = text;
+            this.num = num;
+        }
+    }
+
+
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseClass b = new BaseClass("abc");
+            Console.WriteLine(b.GetText());
+
+            b.Setter();
+            Console.WriteLine(b.GetText());
+
+            b.Setter("new text");
+            Console.WriteLine(b.GetText());
+
+            InheritClass c = new InheritClass(10, "InheritClass text");
+            Console.WriteLine(c.GetText());
+
+            c.Setter("Inherit Text", 20);
+            b = c;
+            Console.WriteLine(b.GetText());
+            Console.WriteLine(c.GetText() + " " + c.num);
+        }
+    }
+}
