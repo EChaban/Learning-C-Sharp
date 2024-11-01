e639117fa50798ff399117f3cca22ac54bb9e9dd
Eugene Chaban <4chaban@gmail.com>
2024-11-01 09:43:40 +0200

    С# Book#2 - Глава 1

---
+++ C# Book №2 - (Васильев А.Н.)/Глава 1. Абстрактные классы и интерфейсы/Homework 1/Homework 1.cs
@@ -0,0 +1,94 @@
+﻿/*Напишите программу, содержащую абстрактный базовый класс с за-
+щищенным полем, являющимся ссылкой на целочисленный массив.
+У класса должен быть конструктор с одним аргументом (определяет
+размер массива и создает его), целочисленное свойство (значение —
+размер массива), абстрактный метод (без аргументов, не возвращает ре-
+зультат) и индексатор с целочисленным индексом (доступен для чтения
+и записи). В производном классе описать абстрактный метод из базового
+класса, чтобы он отображал в консоли содержимое массива. Индексатор
+определить так, чтобы с его помощью можно было прочитать значение
+элемента массива и присвоить значение элементу массива.*/
+
+using System;
+
+namespace Homework_1
+{
+    abstract class Base
+    {
+        protected int[] array;
+
+        public Base(int length)
+        {
+            this.array = new int[length];
+        }
+
+        public int Length { 
+            get { return array.Length; } 
+        }
+        public abstract void Print();
+        public abstract int this[int index]
+        {
+            get;
+            set;
+        }
+
+    }
+
+    class MyClass : Base
+    {
+        public MyClass(int length) : base(length) { }
+
+        public override void Print()
+        {
+            foreach (int item in array)
+            {
+                Console.Write(item + " ");
+            }
+        }
+
+        public override int this[int index]
+        {
+            get
+            {
+                if (index >= 0 && index < array.Length)
+                {
+                    return array[index];
+                }
+                else
+                {
+                    throw new IndexOutOfRangeException("Индекс выходит за пределы массива.");
+                }
+            }
+            set
+            {
+                if (index >= 0 && index < array.Length)
+                {
+                    array[index] = value;
+                }
+                else
+                {
+                    throw new IndexOutOfRangeException("Индекс выходит за пределы массива.");
+                }
+            }
+        }
+    }
+
+    internal class Program
+    {
+        static void Main()
+        {
+            MyClass obj = new MyClass(5);
+
+            for (int i = 0; i < obj.Length; i++)
+            {
+                obj[i] = i;
+            }
+
+            obj.Print();
+
+            Console.WriteLine();
+            obj[0] = 11;
+            Console.WriteLine(obj[0]);
+        }
+    }
+}
---
+++ C# Book №2 - (Васильев А.Н.)/Глава 1. Абстрактные классы и интерфейсы/Homework 2/Homework 2.cs
@@ -0,0 +1,73 @@
+﻿/*Напишите программу, содержащую базовый класс с защищенным
+текстовым полем. У класса должен быть конструктор с текстовым аргументом, 
+доступное для чтения абстрактное целочисленное свойство,
+доступный для чтения абстрактный индексатор с целочисленным индексом, 
+не возвращающий результат абстрактный метод с текстовым аргументом, 
+не возвращающий результат абстрактный метод без аргументов. 
+В производном классе: свойство результатом возвращает количество символов в текстовом поле, 
+значением выражения с проиндексированным объектом является код символа в тексте, 
+метод с текстовым аргументом присваивает новое значение полю, 
+а метод без аргументов отображает значение поля в консольном окне.*/
+
+using System;
+
+namespace Homework_2
+{
+    abstract class BaseClass
+    {
+        protected string text;
+
+        public BaseClass(string text)
+        {
+            this.text = text;
+        }
+
+        public abstract int Length { get; }
+
+        public abstract int this[int index] { get; }
+
+        public abstract void SetText(string newText);
+        public abstract void PrintText();
+    }
+
+    class DerivedClass : BaseClass
+    {
+        public DerivedClass(string text) : base(text) {}
+
+        public override int Length
+        {
+            get { return text.Length; }
+        }
+
+        public override int this[int index]
+        {
+            get { return text[index]; }
+        }
+
+        public override void SetText(string newText)
+        {
+            text = newText;
+        }
+
+        public override void PrintText()
+        {
+            Console.WriteLine(text);
+        }
+    }
+
+    internal class Program
+    {
+        static void Main()
+        {
+            DerivedClass obj = new DerivedClass("Hello");
+            obj.PrintText();
+            obj.SetText("Bye");
+            obj.PrintText();
+            for (int i = 0; i < obj.Length; i++)
+            {
+                Console.Write($"| {(char)obj[i]} ");
+            }
+            Console.Write("|\n");
+        }
+    }
+}
