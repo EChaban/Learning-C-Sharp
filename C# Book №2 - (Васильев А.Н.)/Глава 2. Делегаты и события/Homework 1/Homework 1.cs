b2a552c2bfa529da88a5b38925438217ba1b4247
Eugene Chaban <4chaban@gmail.com>
2024-12-05 12:52:47 +0200

    Глава 2. Делегаты и события

---
+++ C# Book №2 - (Васильев А.Н.)/Глава 2. Делегаты и события/Homework 1/Homework 1.cs
@@ -0,0 +1,39 @@
+﻿/*Напишите программу, в которой объявляется делегат для методов
+с двумя аргументами (символ и текст) и целочисленным результатом.
+В главном классе необходимо описать два статических метода. Один
+статический метод результатом возвращает количество вхождений сим-
+вола (первый аргумент) в текстовую строку (второй аргумент). Другой
+метод результатом возвращает индекс первого вхождения символа (пер-
+вый аргумент) в текстовую строку (второй аргумент) или значение -1,
+если символ в текстовой строке не встречается. В главном методе со-
+здать экземпляр делегата и с помощью этого экземпляра вызвать каж-
+дый из статических методов.*/
+
+using System;
+using System.Linq;
+
+namespace Глава_2.Делегаты_и_события
+{
+    delegate int TestDelegate(char symb, string text);
+
+    internal class Program
+    {
+        static int CountSymbs(char symb, string text)
+        {
+            return text.Count(s => s == symb);
+        }
+
+        static int FirstSymb(char symb, string text)
+        {
+            return text.IndexOf(symb);
+        }
+
+        static void Main(string[] args)
+        {
+            TestDelegate delegat = new TestDelegate(CountSymbs);
+            Console.WriteLine(delegat('a', "ababab"));
+            delegat = FirstSymb;
+            Console.WriteLine(delegat('b', "ababab"));
+        }
+    }
+}
---
+++ C# Book №2 - (Васильев А.Н.)/Глава 2. Делегаты и события/Homework 2/Homework 2.cs
@@ -0,0 +1,52 @@
+﻿/*Напишите программу, в которой объявляется делегат для методов
+с символьным аргументом, не возвращающих результат. Опишите класс,
+в котором должно быть символьное поле и метод, позволяющий присво-
+ить значение символьному полю объекта. У метода один символьный
+аргумент, и метод не возвращает результат. Создайте массив объектов
+данного класса. Создайте экземпляр делегата. В список вызовов этого
+делегата необходимо добавить ссылки на метод (присваивающий значе-
+ние символьному полю) каждого объекта из массива. Проверьте резуль-
+тат вызова такого экземпляра делегата.*/
+
+using System;
+
+delegate void TestDelegate(char symb);
+
+namespace Homework_2
+{
+    class MyClass
+    {
+        char _symb;
+        public void SetText(char symb)
+        {
+            _symb = symb;
+        }
+
+        public char GetText()
+        {
+            return _symb;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass obj1 = new MyClass();
+            MyClass obj2 = new MyClass();
+            MyClass obj3 = new MyClass();
+            MyClass[] myClasses = { obj1, obj2, obj3 };
+            TestDelegate myDelegate;
+            for (int i = 0; i < myClasses.Length; i++)
+            {
+                myDelegate = myClasses[i].SetText;
+                myDelegate((char)('a' + i));
+            }
+
+            foreach (MyClass obj in myClasses)
+            {
+                Console.WriteLine(obj.GetText());
+            }
+        }
+    }
+}
